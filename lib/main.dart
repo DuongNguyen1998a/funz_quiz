25ad1ebe7373e70c512164b99b319fdca4cf0bdc
Duong Nguyen <duongnguyen@MacBook-Pro-cua-Duong.local>
2023-11-19 21:28:17 +0700

    .gitignore is now updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:funz_quiz/router/quiz_router.dart';
 import 'package:funz_quiz/shared/themes/light_theme.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: WidgetsFlutterBinding.ensureInitialized());
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
