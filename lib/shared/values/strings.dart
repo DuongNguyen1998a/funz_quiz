d93ec7c2703c59d4a9a2a39ac194c3ce748714c9
Duong Nguyen <duongnguyen@192.168.0.100>
2023-11-20 23:33:45 +0700

    [imp] splash icon for android12

--- lib/shared/values/strings.dart
+++ lib/shared/values/strings.dart
@@ -3,6 +3,7 @@ class Strings {
   static const String loginTitle = 'Welcome to Quizz ?';
   static const String loginWithGoogle = 'Sign in with Google';
 
+  /// Quiz screen
   static const String quizTitle = 'Play Quizz \n& learn';
   static const String startPlaying = 'Start playing quiz now !!!';
 }
